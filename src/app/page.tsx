500d3db6017ffc5284a2bba0435189de0cc752d8
Adityasputra <aditsputra.cv@gmail.com>
2025-01-28 23:56:44 +0700

    feat: added about and ui tech section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/Navbar";
+import TechSection from "@/components/ui/TechSection";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
       <header className="container mx-auto lg:absolute top-0 left-0 right-0 z-10">
         <Navbar />
       </header>
+
       <main className="container mx-auto min-h-screen flex flex-col lg:flex-row items-center gap-8 mt-8">
         {/* Left Section */}
         <div className="lg:w-1/2 flex flex-col items-center text-center gap-4 px-4 lg:items-start lg:text-left">
@@ -35,6 +37,57 @@ export default function Home() {
           </div>
         </div>
       </main>
+
+      <section className="bg-[#0e1224] py-4 min-h-screen w-full mx-auto">
+        <div className="text-[#F8F7F3] p-10 mx-5 lg:mx-20">
+          <h2 className="text-4xl lg:text-5xl">Hello!</h2>
+          <h1 className="text-5xl lg:text-6xl mt-4">I'm Aditya Saputra</h1>
+          <p className="mt-6">
+            I'm a technology enthusiast passionate about programming, especially
+            in web development. My journey began in high school and continued
+            through completing the Hacktiv8 bootcamp as a full-stack JavaScript
+            developer. I'm always learning new technologies to stay ahead and
+            improve my skills.
+          </p>
+        </div>
+
+        <div className="text-[#F8F7F3] p-10 mx-5 lg:mx-20">
+          <h2 className="text-xl lg:text-2xl">Technology that I use:</h2>
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-6">
+            <TechSection
+              techSection={{
+                title: "Language",
+                technologies: ["Javascript", "Typescript", "Go"],
+              }}
+            />
+            <TechSection
+              techSection={{
+                title: "Frontend & Mobile",
+                technologies: [
+                  "React",
+                  "NextJs",
+                  "Tailwind",
+                  "Redux",
+                  "Vite",
+                  "React Native",
+                ],
+              }}
+            />
+            <TechSection
+              techSection={{
+                title: "Backend & Database",
+                technologies: ["ExpressJs", "NodeJs", "PostgreSQL", "MongoDB"],
+              }}
+            />
+            <TechSection
+              techSection={{
+                title: "Cloud & DevOps",
+                technologies: ["AWS", "Docker", "Git/Github"],
+              }}
+            />
+          </div>
+        </div>
+      </section>
     </>
   );
 }
---
+++ src/components/ui/TechSection.tsx
@@ -0,0 +1,26 @@
+interface TechSection {
+  title: string;
+  technologies: string[];
+}
+
+interface TechSectionProps {
+  techSection: TechSection;
+}
+
+export default function TechSection({ techSection }: TechSectionProps) {
+  return (
+    <div>
+      <p className="font-semibold">{techSection.title}</p>
+      <div className="flex flex-wrap mt-2 gap-2">
+        {techSection.technologies.map((tech) => (
+          <span
+            key={tech}
+            className="bg-gradient-to-r from-[#FEB143] to-[#b9a87d] px-3 py-1 rounded-md text-sm"
+          >
+            {tech}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+}
