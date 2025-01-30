9875948b50819c10d0c3ab2605ab5020caccedc5
Adityasputra <aditsputra.cv@gmail.com>
2025-01-31 00:24:50 +0700

    fix: fixed next seo configuration

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -1,19 +1,19 @@
-// next-seo.config.ts
 import { NextSeoProps } from "next-seo";
 
 const SEO: NextSeoProps = {
   title: "Aditya Saputra | Developer Portfolio",
+  titleTemplate: "%s | Aditya Saputra",
   description:
     "Official portfolio of Aditya Saputra, a full stack javascript developer.",
   openGraph: {
     type: "website",
     locale: "id_ID",
-    url: "https://0be3-114-5-223-237.ngrok-free.app",
+    url: "https://www.adityasputra.my.id/",
     title: "Aditya Saputra | Developer Portfolio",
     description: "Explore recent experiences, projects and posts.",
     images: [
       {
-        url: "https://0be3-114-5-223-237.ngrok-free.app/images/display.jpeg",
+        url: "https://www.adityasputra.my.id/images/display.jpeg",
         width: 1200,
         height: 630,
         alt: "Aditya Saputra Portfolio",
@@ -21,6 +21,13 @@ const SEO: NextSeoProps = {
     ],
     site_name: "Aditya Saputra",
   },
+  additionalMetaTags: [
+    {
+      name: "keywords",
+      content: "portfolio, developer, full stack, javascript, Aditya Saputra",
+    },
+  ],
+  defaultTitle: "Aditya Saputra",
 };
 
 export default SEO;
