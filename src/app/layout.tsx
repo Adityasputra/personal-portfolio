655a4bb7144633932685df4b49812c55aab05ac9
Adityasputra <aditsputra.cv@gmail.com>
2025-01-31 01:19:17 +0700

    feat: added icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { metadata } from "./metadata";
+import { Metadata } from "next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,7 +14,26 @@ const geistMono = Geist_Mono({
   display: "swap",
 });
 
-export { metadata };
+export const metadata: Metadata = {
+  title: "Aditya Saputra | Developer Portfolio",
+  description:
+    "Official portfolio of Aditya Saputra, a full stack javascript developer.",
+  keywords: [
+    "Next.js",
+    "React",
+    "Frontend Developer",
+    "Full Stack Javascript Developer",
+    "Backend Developer",
+    "Portfolio",
+  ],
+  authors: [{ name: "Aditya Saputra", url: "https://github.com/Adityasputra" }],
+  openGraph: {
+    title: "Aditya Saputra | Developer Portfolio",
+    description: "Explore recent experiences, projects and posts.",
+    type: "website",
+    url: "https://www.adityasputra.my.id/",
+  },
+};
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
