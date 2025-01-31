4143f14e0f6cd4245d80fcfcaf713728238c3c70
Adityasputra <aditsputra.cv@gmail.com>
2025-01-31 14:21:04 +0700

    feat : added condition for app url null or not

--- src/components/ui/SocialLink.tsx
+++ src/components/ui/SocialLink.tsx
@@ -2,8 +2,8 @@ import { FaGithub } from "react-icons/fa";
 import { CgWebsite } from "react-icons/cg";
 
 interface SocialLinks {
-  githubUrl: string;
-  websiteUrl: string;
+  githubUrl?: string | null;
+  websiteUrl?: string | null;
 }
 
 interface SocialLinksProps {
@@ -14,12 +14,20 @@ export default function SocialLinks({ project }: SocialLinksProps) {
   return (
     <div>
       <div className="flex gap-2">
-        <a href={project.githubUrl} target="_blank" rel="noopener noreferrer">
-          <FaGithub size={24} />
-        </a>
-        <a href={project.websiteUrl} target="_blank" rel="noopener noreferrer">
-          <CgWebsite size={24} />
-        </a>
+        {project.githubUrl && (
+          <a href={project.githubUrl} target="_blank" rel="noopener noreferrer">
+            <FaGithub size={24} />
+          </a>
+        )}
+        {project.websiteUrl && (
+          <a
+            href={project.websiteUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <CgWebsite size={24} />
+          </a>
+        )}
       </div>
     </div>
   );
