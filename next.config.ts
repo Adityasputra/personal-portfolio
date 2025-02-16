df50e6384bf8fcd9de245591a386fb221d5db780
Adityasputra <aditsputra.cv@gmail.com>
2025-02-16 23:01:02 +0700

    feat: adding bundle analyzer for analyzer chunck

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
