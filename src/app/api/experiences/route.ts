b0735580b31c8e1b8fda7756fc7bed5841dcd32a
Adityasputra <aditsputra.cv@gmail.com>
2025-02-17 16:39:09 +0700

    feat: adding server error sumulation in experiences api

--- src/app/api/experiences/route.ts
+++ src/app/api/experiences/route.ts
@@ -4,3 +4,9 @@ import experiences from "@/data/experiences.json";
 export async function GET() {
   return NextResponse.json(experiences, { status: 200 });
 }
+
+// export async function GET() {
+//   return new Response(JSON.stringify({ message: "Server error" }), {
+//     status: 500, // Server error simulation
+//   });
+// }
