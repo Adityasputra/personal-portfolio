407f3eabc36101943e99fa79d4fede2000b2dd5a
Adityasputra <aditsputra.cv@gmail.com>
2025-02-17 17:58:02 +0700

    feat: adding use memo in card components

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,33 +1,28 @@
+import React from "react";
 import Image from "next/image";
 import SocialLinks from "./SocialLink";
+import { Project } from "@/types";
 
-interface Project {
-  id: number;
-  name: string;
-  description: string;
-  repoUrl?: string | null;
-  appUrl?: string | null;
-  imageUrl?: string | null;
-  technologies: string[];
-  star: boolean;
-}
-
-interface CardProps {
+interface ProjectProps {
   project: Project;
 }
 
-export default function Card({ project }: CardProps) {
+const Card = React.memo(({ project }: ProjectProps) => {
+  const { name, imageUrl, description, technologies, repoUrl, appUrl } =
+    project;
+
   return (
-    <div className="flex flex-col md:h-full lg:h-full bg-white shadow-md rounded-lg overflow-hidden transition-transform duration-300 ease-in-out hover:scale-105">
+    <div className="flex flex-col bg-white shadow-md rounded-lg overflow-hidden transition-transform duration-300 ease-in-out hover:scale-105">
       {/* Image Section */}
       <div className="relative w-full h-64">
         <Image
-          src={project.imageUrl || "/images/dummy.webp"}
-          alt={`Image of ${project.name}`}
+          src={imageUrl || "/images/dummy.webp"}
+          alt={`Image of ${name}`}
           fill
-          priority
+          // priority
           sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
           className="rounded-t-lg object-cover object-center"
+          loading="lazy"
         />
       </div>
 
@@ -35,16 +30,16 @@ export default function Card({ project }: CardProps) {
       <div className="p-6">
         <div className="flex justify-between items-start">
           <div className="flex-1">
-            <h3 className="text-xl font-bold text-gray-800">{project.name}</h3>
+            <h3 className="text-xl font-bold text-gray-800">{name}</h3>
             <p className="mt-2 text-sm text-gray-600 leading-relaxed line-clamp-4 lg:line-clamp-5">
-              {project.description}
+              {description || "Description not available."}
             </p>
           </div>
           <div className="ml-4">
             <SocialLinks
               project={{
-                githubUrl: project.repoUrl,
-                websiteUrl: project.appUrl,
+                repoUrl: repoUrl || "",
+                appUrl: appUrl || "",
               }}
             />
           </div>
@@ -52,16 +47,24 @@ export default function Card({ project }: CardProps) {
 
         {/* Technologies Section */}
         <div className="mt-4 flex flex-wrap gap-2">
-          {project.technologies.map((tech) => (
-            <span
-              key={tech}
-              className="bg-gradient-to-r from-yellow-400 to-yellow-600 text-white text-xs font-medium px-3 py-1 rounded-md"
-            >
-              {tech}
+          {technologies.length > 0 ? (
+            technologies.map((tech) => (
+              <span
+                key={tech}
+                className="bg-gradient-to-r from-yellow-400 to-yellow-600 text-white text-xs font-medium px-3 py-1 rounded-md"
+              >
+                {tech}
+              </span>
+            ))
+          ) : (
+            <span className="text-sm text-gray-500">
+              No technologies listed
             </span>
-          ))}
+          )}
         </div>
       </div>
     </div>
   );
-}
+});
+
+export default Card;
