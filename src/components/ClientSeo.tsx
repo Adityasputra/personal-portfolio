e2defdad5ad69f1f972dac68a464167798903390
Adityasputra <aditsputra.cv@gmail.com>
2025-02-18 13:16:25 +0700

    feat: adding client seo wrapper for fix next seo components

--- src/components/ClientSeo.tsx
+++ src/components/ClientSeo.tsx
@@ -1,8 +1,18 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { DefaultSeo } from "next-seo";
 import SEO from "../../next-seo.config";
 
 export default function ClientSeo() {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+    console.log("ClientSeo loaded!"); // Debugging
+  }, []);
+
+  if (!mounted) return null;
+
   return <DefaultSeo {...SEO} />;
 }
---
+++ src/components/ClientSeoWrapper.tsx
@@ -0,0 +1,7 @@
+"use client"; // Pastikan ini client component
+
+import ClientSeo from "./ClientSeo";
+
+export default function ClientSeoWrapper() {
+  return <ClientSeo />;
+}
