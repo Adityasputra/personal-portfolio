4be237cb50e0d2ce1bf13d646a03734e13a6fa5a
Adityasputra <aditsputra.cv@gmail.com>
2025-02-23 03:11:15 +0700

    ffix: fixed api for blog by slug

--- src/app/api/blogs/[slug]/route.ts
+++ src/app/api/blogs/[slug]/route.ts
@@ -1,16 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
 import blogs from "@/data/blogs.json";
-import { Blog } from "@/types";
 
-export function GET(request: NextRequest) {
-  const { searchParams } = new URL(request.url);
-  const slug = searchParams.get("slug");
+export async function GET(
+  request: NextRequest,
+  context: { params?: { slug?: string } }
+) {
+  const slug = context.params?.slug;
 
   if (!slug) {
-    return NextResponse.json({ message: "Slug is required" }, { status: 400 });
+    return NextResponse.json({ message: "Missing slug" }, { status: 400 });
   }
 
-  const blog: Blog | undefined = blogs.find((b) => b.slug === slug);
+  const blog = blogs.find((b) => b.slug === slug);
 
   if (!blog) {
     return NextResponse.json({ message: "Blog not found" }, { status: 404 });
