6bcff583852e3c5ad5fecb6a9ecc45f3146d5c5f
Adityasputra <aditsputra.cv@gmail.com>
2025-02-26 01:46:54 +0700

    feat: adding key and type annotation in interface project

--- src/types/project.ts
+++ src/types/project.ts
@@ -2,9 +2,12 @@ export interface Project {
   id: number;
   name: string;
   description: string;
+  shortDescription: string;
   technologies: string[];
   repoUrl: string;
-  appUrl?: string | null;
-  imageUrl?: string | null;
-  star: boolean;
+  appUrl?: string;
+  imageUrl?: string;
+  images?: string[];
+  featured: boolean;
+  status: "ongoing" | "completed" | "paused";
 }
