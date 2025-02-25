3bd1b791f6d38353f11c72ae32372adcb9020514
Adityasputra <aditsputra.cv@gmail.com>
2025-02-26 02:59:41 +0700

    feat: adding api for get project detail by id

--- src/app/api/projects/route.ts
+++ src/app/api/projects/route.ts
@@ -1,6 +1,18 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import projects from "@/data/projects.json";
 
-export async function GET() {
-  return NextResponse.json(projects, { status: 200 });
+export async function GET(req: NextRequest) {
+  const id = req.nextUrl.searchParams.get("id");
+
+  if (id) {
+    const project = projects.find((p) => p.id === parseInt(id));
+    if (!project)
+      return NextResponse.json(
+        { message: "Project not found" },
+        { status: 404 }
+      );
+    return NextResponse.json(project);
+  }
+
+  return NextResponse.json(projects);
 }
